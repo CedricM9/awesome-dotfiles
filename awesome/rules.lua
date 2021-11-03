14c14f87ee7bff8412a31756a7c556d879bf02be
cedricm9 <cedricmaume@gmail.com>
2021-11-03 15:19:09 +0100

    Added Titlebars to all windows

--- awesome/rules.lua
+++ awesome/rules.lua
@@ -40,7 +40,8 @@ function rules.create(clientkeys, clientbuttons)
             keys = clientkeys,
             buttons = clientbuttons,
             screen = awful.screen.preferred,
-            placement = awful.placement.centered
+            placement = awful.placement.centered,
+            titlebars_enabled = true
 
          },
       },
