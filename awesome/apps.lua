31b81c22d49201fdeffe3cba40e7b066511342ae
cedricm9 <cedricmaume@gmail.com>
2021-11-11 00:12:41 +0100

    Rofi Theme is now selected from Non User Environment

--- awesome/apps.lua
+++ awesome/apps.lua
@@ -26,7 +26,7 @@ local apps = {}
 -- define default apps
 apps.default = {
    terminal = "alacritty",
-   launcher = "rofi -normal-window -modi drun -show drun",
+   launcher = "rofi -show drun -theme custom",
    lock = "i3lock -c 00000000 --blur 5",
    --screenshot = "scrot ~/Desktop/%b\ %d\ -\ %H:%M.png",
    filebrowser = "nautilus",
